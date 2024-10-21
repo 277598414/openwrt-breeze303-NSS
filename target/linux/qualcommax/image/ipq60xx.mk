2953beefc0dd6f931846b98488d11e42ea855d7d
breeze303 <grapenskrskr@gmail.com>
2024-10-21 18:06:27 +0800

    ipq60xx: fix

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -64,7 +64,7 @@ define Device/jdcloud_ax1800-pro
 	DEVICE_VENDOR := JDCloud
 	DEVICE_MODEL := AX1800 Pro
 	DEVICE_DTS_CONFIG := config@cp03-c2
-	DEVICE_DTS := ipq6018-jdcloud-ax1800-pro
+	DEVICE_DTS := ipq6000-jdcloud-ax1800-pro
 	SOC := ipq6000
 	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax1800-pro kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
 	BLOCKSIZE := 64k
@@ -79,7 +79,7 @@ define Device/jdcloud_ax6600
 	DEVICE_VENDOR := JDCloud
 	DEVICE_MODEL := AX6600
 	DEVICE_DTS_CONFIG := config@cp03-c3
-	DEVICE_DTS := ipq6018-jdcloud-ax6600
+	DEVICE_DTS := ipq6010-jdcloud-ax6600
 	SOC := ipq6010
 	DEVICE_PACKAGES := ipq-wifi-jdcloud_ax6600 kmod-ath11k-pci ath11k-firmware-qcn9074 kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
 	BLOCKSIZE := 64k
